64a3e7e2ecc2b3734cf654b5ca9bb90b587a7b30
Nkemakolam Nwokoro <40966379+nicholas1989@users.noreply.github.com>
2024-06-06 16:54:20 +0200

    Corrections were made to the script

--- data_hierarchical.r
+++ data_hierarchical.r
@@ -19,10 +19,10 @@ winequality_data_set <- na.omit(winequality_data_set)
 # The scale() function is centering and scaling the data! This is particularly useful when working with algorithm
 # that are sensitive to the scaling of variables 
 
-winequality_data_set <- scale(winequality_data_set)
+winequality_data_set_scaled <- scale(winequality_data_set)
 
 # view the first 6 rows of the dataset
-winequality_data_set <- head(winequality_data_set)
+# winequality_data_set <- head(winequality_data_set)
 # print(winequality_data_set)
 
 # Since we don't know beforehand which method will produce the best clusters, we will write a short function
@@ -36,7 +36,7 @@ names(wine_quality) <- c("average", "single", "complete", "ward")
 
 # function to compute agglomerative coefficient
 quality_red <- function(x) {
-    agnes(winequality_data_set, method = x)$ac
+    agnes(winequality_data_set_scaled, method = x)$ac
 }
 
 # calculate agglomerative coefficient for each clustering linkage method
@@ -47,7 +47,7 @@ print(agglo_quality)
 # agglomerative coefficient, thus we will use that as the method for our final hierarchical clustering.
 
 # perform hierarchical clustering using Ward's minimum variance 
-hierarchical_quality <- agnes(winequality_data_set, method = "ward")
+hierarchical_quality <- agnes(winequality_data_set_scaled, method = "ward")
 
 # Plot the dendrogram
 hq <- pltree(hierarchical_quality, cex = 0.6, hang = -1, main = "Dendrogram - Ward's Method")
@@ -59,44 +59,31 @@ hq <- pltree(hierarchical_quality, cex = 0.6, hang = -1, main = "Dendrogram - Wa
 # `gap statistics`, which compares the total intra-cluster variation for different values of k with their 
 # expected values for a distribution with no clustering.
 
-# calculate gap statistic for each number of clusters (up to 5 clusters)
-quality_stats <- clusGap(winequality_data_set, FUN = hcut, nstart = 25, K.max = 5, B = 50)
+# calculate gap statistic for each number of clusters (up to 10 clusters)
+set.seed(123)
+quality_stats <- clusGap(winequality_data_set_scaled, FUN = hcut, nstart = 25, K.max = 10, B = 50)
 
 # produce plot of clusters vs. gap statistic
-quality_stats <- fviz_gap_stat(quality_stats)
-print(quality_stats)
+fviz_gap_stat(quality_stats)
 
-# From the plot we can see that the gap statistics is the highest at k = 5 clusters. Thus, we'll choose to 
-# group our observations into 5 distinct clusters.
-# To actually add cluster labels to each observation in our dataset, we can use the `cutree()` method to cut 
-# the dendrogram into 5 clusters
+# Determine the optimal number of clusters
+gap_df <- as.data.frame(quality_stats$Tab)
+optimal_k <- which.max(gap_df$gap)
+print(paste("Optimal number of clusters:", optimal_k))
 
-# compute distance matrix
-d_quality <- dist(winequality_data_set, method = "euclidean")
+# Perform final hierarchical clustering using Ward's method
+final_clust <- hclust(dist(winequality_data_set_scaled, method = "euclidean"), method = "ward.D2")
 
-# perform hierarchical clustering using Ward's method
-final_clust <- hclust(d_quality, method = "ward.D2")
-# print(final_clust)
-
-# cut the dendrogram into 5 clusters
-cluster_wine <- cutree(final_clust, k=5)
-print(cluster_wine)
-
-# find number of observations in each cluster
+# Cut the dendrogram into the optimal number of clusters
+cluster_wine <- cutree(final_clust, k = optimal_k)
 print(table(cluster_wine))
 
-
-# We can then append the cluster labels of each attributes back to the original dataset.
-
-# append cluster labels to original data
+# Append cluster labels to original data
 final_data <- as.data.frame(cbind(winequality_data_set, cluster = cluster_wine))
 
-#ndisplay first six rows of the final data
-head_quality <- head(final_data)
-# print(head_quality)
+# Display first six rows of the final data
+head(final_data)
 
-# Lastly, we find the mean of the variables in each cluster by using the `aggregate()` function
-
-# find mean values for each cluster
+# Find mean values for each cluster
 cluster_means <- aggregate(. ~ cluster, data = final_data, mean)
 print(cluster_means)
